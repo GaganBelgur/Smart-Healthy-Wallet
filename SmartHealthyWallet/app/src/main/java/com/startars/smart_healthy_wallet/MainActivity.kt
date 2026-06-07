abbafef1acf9722ffa115ab8587870f5d9f5a0bb
GaganBelgur <gagan.belgur48@gmail.com>
2026-06-07 17:00:52 +0530

    Add initial project structure and configuration files

---
+++ SmartHealthyWallet/app/src/main/java/com/startars/smart_healthy_wallet/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.startars.smart_healthy_wallet
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.startars.smart_healthy_wallet.ui.theme.SmartHealthyWalletTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SmartHealthyWalletTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SmartHealthyWalletTheme {
+        Greeting("Android")
+    }
+}
